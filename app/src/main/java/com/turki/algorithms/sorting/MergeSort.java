209fe101b8ce8831073d92d2bfba4add691a27de
mahmoud-turki <Mahmoud.Turki92@gmail.com>
2017-08-18 16:53:50 -0500

    Commit merge and quick sort

--- app/src/main/java/com/turki/algorithms/sorting/MergeSort.java
+++ app/src/main/java/com/turki/algorithms/sorting/MergeSort.java
@@ -1,7 +1,25 @@
 package com.turki.algorithms.sorting;
 
 /**
- *
+ * Like QuickSort, Merge Sort is a Divide and Conquer algorithm. It divides input array in two halves,
+ * calls itself for the two halves and then merges the two sorted halves.
+ * <p>
+ * The merge() function is used for merging two halves. The merge(arr, l, m, r) is key process that assumes
+ * that arr[l..m] and arr[m+1..r] are sorted and merges the two sorted sub-arrays into one.
+ * <p>
+ * <p>
+ * MergeSort(arr[], l,  r)
+ * If r > l
+ * 1. Find the middle point to divide the array into two halves:
+ * middle m = (l+r)/2
+ * 2. Call mergeSort for first half:
+ * Call mergeSort(arr, l, m)
+ * 3. Call mergeSort for second half:
+ * Call mergeSort(arr, m+1, r)
+ * 4. Merge the two halves sorted in step 2 and 3:
+ * Call merge(arr, l, m, r)
+ * <p>
+ * <p>
  * Analysis:
  * <p>
  * Best Ω(n log(n)) - Average θ(n log(n)) - Worst O(n log(n)).
@@ -10,15 +28,68 @@ package com.turki.algorithms.sorting;
  */
 public class MergeSort {
 
-    public void mergeSort(int array[]) {
-        // TODO: Will be published on 19th, August, 2017
+    /**
+     * @param array
+     * @param low
+     * @param high
+     */
+    public void mergeSort(int array[], int low, int high) {
+        // check if low is smaller than high, if not then the array is sorted
+        if (low < high) {
+            // Get the index of the element which is in the middle
+            int middle = low + (high - low) / 2;
+            // Sort the left side of the array
+            mergeSort(array, low, middle);
+            // Sort the right side of the array
+            mergeSort(array, middle + 1, high);
+            // Combine them both
+            merge(array, low, middle, high);
+        }
     }
+
+    /**
+     * @param array
+     * @param low
+     * @param middle
+     * @param high
+     */
+    private void merge(int array[], int low, int middle, int high) {
+        int helper[] = new int[array.length];
+
+        // Copy both parts into the helper array
+        for (int i = low; i <= high; i++) {
+            helper[i] = array[i];
+        }
+
+        int i = low, j = middle + 1, k = low;
+
+        // Copy the smallest values from either the left or the right side back to the original array
+        while (i <= middle && j <= high) {
+            if (helper[i] <= helper[j]) {
+                array[k] = helper[i];
+                i++;
+            } else {
+                array[k] = helper[j];
+                j++;
+            }
+            k++;
+        }
+        // Copy the rest of the left side of the array into the target array
+        while (i <= middle) {
+            array[k] = helper[i];
+            k++;
+            i++;
+        }
+        // Since we are sorting in-place any leftover elements from the right side are already at the right position.
+
+    }
+
 
     public static void main(String args[]) {
 
         MergeSort sort = new MergeSort();
         int arr[] = {5, 1, 3, 2, 4};
-        sort.mergeSort(arr);
+        sort.mergeSort(arr, 0, arr.length-1);
         System.out.println("Sorted array");
         for (int x : arr) {
             System.out.println(x);
--- app/src/main/java/com/turki/algorithms/sorting/QuickSort.java
+++ app/src/main/java/com/turki/algorithms/sorting/QuickSort.java
@@ -1,7 +1,30 @@
 package com.turki.algorithms.sorting;
 
 /**
- *
+ * Like Merge Sort, QuickSort is a Divide and Conquer algorithm.
+ * It picks an element as pivot and partitions the given array around the picked pivot.
+ * <p>
+ * There are many different versions of quickSort that pick pivot in different ways.
+ * 1- Always pick first element as pivot.
+ * 2- Always pick last element as pivot (implemented below)
+ * 3- Pick a random element as pivot.
+ * 4- Pick median as pivot.
+ * <p>
+ * The key process in quickSort is partition().
+ * Target of partitions is, given an array and an element x of array as pivot,
+ * put x at its correct position in sorted array and put all smaller elements (smaller than x) before x,
+ * and put all greater elements (greater than x) after x. All this should be done in linear time.
+ * <p>
+ * low  --> Starting index,  high  --> Ending index
+ * quickSort(arr[], low, high)
+ * {
+ * if (low < high){
+ * //pi is partitioning index, arr[p] is now at right place
+ * pi = partition(arr, low, high);
+ * quickSort(arr, low, pi - 1);  // Before pi
+ * quickSort(arr, pi + 1, high); // After pi
+ * }
+ * }
  * Analysis:
  * <p>
  * Best Ω(n log(n)) - Average θ(n log(n)) - Worst O(n^2).
@@ -10,15 +33,62 @@ package com.turki.algorithms.sorting;
  */
 public class QuickSort {
 
-    public void quickSort(int array[]) {
-        // TODO: Will be published on 19th, August, 2017
+    /**
+     * @param array
+     * @param low
+     * @param high
+     */
+    public void quickSort(int array[], int low, int high) {
+
+        if (low < high) {
+            //pi is partitioning index, arr[pi] is now at right place
+            int partitionIndex = partition(array, low, high);
+
+            // Recursively sort elements before partition and after partition
+            quickSort(array, low, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, high);
+        }
+    }
+
+
+    /**
+     * This function takes last element as pivot, places the pivot element at its correct position in sorted array,
+     * and places all smaller (smaller than pivot) to left of pivot and all greater elements to right of pivot.
+     *
+     * @param array
+     * @param low
+     * @param high
+     * @return
+     */
+    private int partition(int array[], int low, int high) {
+        int pivot = array[high];
+        // index of smaller element
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            // If current element is smaller than or equal to pivot
+            if (array[j] <= pivot) {
+                i++;
+
+                // swap array[i] and array[j]
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+
+        // swap array[i+1] and array[high] (or pivot)
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+
+        return i + 1;
     }
 
     public static void main(String args[]) {
 
         QuickSort sort = new QuickSort();
         int arr[] = {5, 1, 3, 2, 4};
-        sort.quickSort(arr);
+        sort.quickSort(arr, 0, arr.length - 1);
         System.out.println("Sorted array");
         for (int x : arr) {
             System.out.println(x);
